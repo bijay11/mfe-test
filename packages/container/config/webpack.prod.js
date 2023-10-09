e50c4212754e3522b9bd8fe322337c7c81c5896c
Bijay Tamang <tmg.bijay11@gmail.com>
2023-10-09 13:32:56 -0400

    setup prod webpack for container project

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    // contenthash - typically for caching issues.
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      // name for the host is optional
+      name: "container",
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
